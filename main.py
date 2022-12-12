a0224ecdafcf64349b554f4010135c6051869ae6
Alexander Carpio Mamani <62957804+dretcm@users.noreply.github.com>
2022-12-12 18:01:57 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import mysql.connector
+import itertools
 from PyQt5 import uic, QtWidgets, QtCore
-from PyQt5.QtWidgets import (QApplication, QMainWindow,QLabel,QPushButton, 
+from PyQt5.QtWidgets import (QApplication, QMainWindow,QLabel,QPushButton, QWidget,
                             QTableWidget,QTableWidgetItem, QComboBox,QLineEdit,QPushButton)
 import sys
 
 hostname = 'localhost'
 username = 'root'
 password = 'root'
-database = 'sakila'
+database = 'mydb'
 
 myConnection = mysql.connector.connect( 
     host=hostname, 
@@ -17,13 +18,45 @@ myConnection = mysql.connector.connect(
 
 cur = myConnection.cursor()
 
-def Select_all_Query(table) :
-    cur.execute( f"SELECT * FROM {table}" )
+def Select_all_Query(table):
+    cur.execute( f"SELECT * FROM {table}")
     return cur.fetchall()
 
-def Select_where_Query(table, column, text) :
+def Select_where_Query(table, column, text):
+    #print(f"SELECT * FROM {table} WHERE {column}='{text}'")
     cur.execute(f"SELECT * FROM {table} WHERE {column}='{text}'")
     return cur.fetchall()
+
+def insert_Query(table, cols, data):
+    if data:
+        try:
+            n = ("%s," * len(cols))[:-1]
+            cols = "(" + ",".join(cols) + ")"
+            #print(f"INSERT INTO {table} {cols} VALUES ({n})")
+            cur.executemany(f"INSERT INTO {table} {cols} VALUES ({n})", data)
+            myConnection.commit()
+
+def remove_Query(table, cols, data):
+    if data:
+        try:
+            for sub_data in data:
+                cad = ""
+                for i in range(len(cols)):
+                    cad += cols[i] + "=" + "'" + sub_data[i] + "'" + " AND "
+                cad = cad[:-5]
+                cur.execute(f"DELETE FROM {table} WHERE {cad}")
+            myConnection.commit()
+
+
+def get_combinations(values):
+    output = values.copy()
+    output.insert(0, "*")
+    for i in range(2,len(values)):
+        com = list(itertools.combinations(values,r=i))
+        for j in com:
+            data = ",".join(j)
+            output.append(data)
+    return output
 
 
 class TableView(QTableWidget):
@@ -33,7 +66,11 @@ class TableView(QTableWidget):
 
     def udpate_tablewidget(self, data, x, y):
         self.data = data
-        self.setRowCount(x)
+        self.data_querys = []
+        self.data_removes = {}
+        self.size_columns_ = y
+        self.size_rows_ = x+1
+        self.setRowCount(self.size_rows_)
         self.setColumnCount(y)
         self.setData()
         self.resizeColumnsToContents()
@@ -41,19 +78,43 @@ class TableView(QTableWidget):
  
     def setData(self): 
         horHeaders = []
-        for n, key in enumerate(sorted(self.data.keys())):
+        for n, key in enumerate(list(self.data.keys())):
             horHeaders.append(key)
             for m, item in enumerate(self.data[key]):
                 newitem = QTableWidgetItem(item)
                 self.setItem(m, n, newitem)
         self.setHorizontalHeaderLabels(horHeaders)
 
+    def new_row_widget(self):
+        self.size_rows_ += 1
+        self.setRowCount(self.size_rows_)
+        self.resizeColumnsToContents()
+        self.resizeRowsToContents()
+
+        querys = []
+        for i in range(self.size_columns_):
+            querys.append(self.item(self.size_rows_-2, i).text())
+        self.data_querys.append(querys)
+        print(self.data_querys)
+
+    def remove_row_widget(self):
+        n = self.currentRow()
+        if n not in self.data_removes:
+            self.data_removes[n] = [self.item(n, i).text() for i in range(self.size_columns_)]
+        print(self.data_removes)
+
+class Input_Table(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.label = QLabel("Jola",self)
+        self.move(150,150)
+
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
   
-        self.setWindowTitle("Python")
+        self.setWindowTitle("Visualizer")
 
         self.setGeometry(50, 50, 1080, 620)
 
@@ -67,7 +128,7 @@ class Window(QMainWindow):
         label.setGeometry(240, 10, 200, 30)
 
         self.button = QPushButton("SELECT * FROM", self)
-        self.button.setStyleSheet('font: 75 11pt "Arial";')
+        self.button.setStyleSheet('font: 75 12pt "Arial";')
         self.button.setGeometry(20, 50, 200, 30)
         self.button.clicked.connect(self.update_table)
 
@@ -88,6 +149,7 @@ class Window(QMainWindow):
         self.col = QComboBox(self)
         self.col.setGeometry(240, 130, 200, 30)
         self.col.setStyleSheet('font: 75 11pt "Arial";')
+        self.col_items = []
 
         label = QLabel("<b>=</b>", self)
         label.setStyleSheet('font: 80 11pt "Arial";')
@@ -104,6 +166,22 @@ class Window(QMainWindow):
         self.button_search.setGeometry(20, 130, 200, 30)
         self.button_search.clicked.connect(lambda: self.update_table(where=True))
 
+
+        self.more = QPushButton("+", self)
+        self.more.setStyleSheet('font: 80 12pt "Arial";')
+        self.more.setGeometry(860, 10, 100, 30)
+        self.more.clicked.connect(self.add_row)
+
+        self.less = QPushButton("-", self)
+        self.less.setStyleSheet('font: 100 16pt "Arial";')
+        self.less.setGeometry(860, 50, 100, 30)
+        self.less.clicked.connect(self.remove_row)
+
+        self.save = QPushButton("Save", self)
+        self.save.setStyleSheet('font: 75 11pt "Arial";')
+        self.save.setGeometry(970, 10, 100, 30)
+        self.save.clicked.connect(self.save_all)
+
         cur.execute("SHOW TABLES")
         for i in cur.fetchall():
             self.option.addItem(i[0])
@@ -111,12 +189,14 @@ class Window(QMainWindow):
         self.table = None
         self.update_table()
 
-
     def update_table(self, where=False):
-        op = str(self.option.currentText())
-        dataframe, x, y, columns = self.get_table(op, where)
+        cur.execute(f"show columns from {self.option.currentText()}")
+        self.col_items = [i[0] for i in cur.fetchall()]
+
+        dataframe, x, y = self.get_table(self.option.currentText(),where, self.col_items)
+
         self.col.clear()
-        self.col.addItems(columns)
+        self.col.addItems(self.col_items)
 
         if self.table:
             self.table.clear()
@@ -126,11 +206,7 @@ class Window(QMainWindow):
             self.table.setGeometry(10, 180, 1060, 430)          
         
 
-    def get_table(self, table, where):
-        cur.execute(f"show columns from {table}")
-        query = cur.fetchall()
-        #print(query)
-        columns = [i[0] for i in query]
+    def get_table(self, table, where, columns):
         if where:
             text = Select_where_Query(table,
                 self.col.currentText(), 
@@ -147,11 +223,26 @@ class Window(QMainWindow):
 
         dataframe = dict(zip(columns, data))
         x,y = len(text), len(columns)
-        return dataframe, x, y, columns
+        return dataframe, x, y
 
     def on_text_changed(self, text):
         width = self.search_input.fontMetrics().width(text)
         self.search_input.setMinimumWidth(width)
+
+    def add_row(self):
+        self.table.new_row_widget()
+
+    def remove_row(self):
+        self.table.remove_row_widget()
+
+    def save_all(self):
+        insert_Query(self.option.currentText(),
+            self.col_items,
+            self.table.data_querys)
+        remove_Query(self.option.currentText(),
+            self.col_items, 
+            list(self.table.data_removes.values()))
+        print(321)
 
 
 if __name__ == '__main__':
